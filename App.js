55590cad70c1a12d3ebadb57aa8cbb5a31f4126b
Oliver Kokolee <ollie12245@gmail.com>
2022-10-31 10:46:45 -0400

    View and Add List Item

--- App.js
+++ App.js
@@ -25,6 +25,12 @@ const App: () => Node = () => {
   } catch (error) {
     console.log('Failed to create items table ' + error);
   }
+
+  try {
+    database.createListItemsTable();
+  } catch (error) {
+    console.log('Failed to create list items table ' + error);
+  }
   return <Router />;
 };
 
---
+++ src/screens/AddListItem/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TextInput, Pressable } from 'react-native';
+import styles from './styles';
+
+const AddListItemScreen = props => {
+
+  return (
+    <View style={styles.container}>
+    </View>
+  );
+};
+
+export default AddListItemScreen;
